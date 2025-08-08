683622b9503778b60e20875232bbcb6912860cff
udittmaheshwari <uditmaheshwari75@gmail.com>
2025-08-08 20:44:33 +0530

    added a logo image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <img src='https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-black-and-white.png' className='h-12 w-6s'  alt='logo'/>
   )
 }
 
